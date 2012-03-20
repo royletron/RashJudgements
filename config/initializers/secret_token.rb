b0d2ce7c2d97b3ed2b58ee78a9411724a5067216
Darren Royle <darren.royle@oup.com>
2012-03-20 09:34:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RashJudgements::Application.config.secret_token = 'ee17861ed3c8bb9762102ee2df81646150d5ffa8fd6a1bb7fb6e36759a078bc072205553b0fb2c331b1f1001d0900a8ae6d175cf039931a98e1c81289752360a'
